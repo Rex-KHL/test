dffcfce0e4a666aad8547076fa534d00530574fe
Guangwei Zhao <guangwei.zhao@strath.ac.uk>
2023-03-01 14:05:01 +0000

    update version

--- frame.m
+++ frame.m
@@ -1,6 +1,6 @@
 vid=VideoReader('A001C0008_20220919085101_0001.MP4');
 n=vid.NumberOfFrames;
- for i=3
- frames=read(vid,i);
- imwrite(frames,['laser',num2str(i,'%04d'), '.jpg']);
- end
+for i=3
+	frames=read(vid,i);
+	imwrite(frames,['laser',num2str(i,'%04d'), '.jpg']);
+end
